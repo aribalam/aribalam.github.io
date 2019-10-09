62caadf160046192bc17bc2a5d155dcfaa5db146
Arib Alam <aribalam64@gmail.com>
2019-10-09 22:33:10 +0530

    added code for page sections

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+    
     $('.header').height($(window).height());
-    console.log("Hello");
+
+    // Set the section content height to full height
+    console.log(window.innerHeight);
+    $('.section_content').height(window.innerHeight);
+    $('.section_content').css('padding', 30);
 });
