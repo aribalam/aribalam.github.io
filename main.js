642fdef9ca738f4cb5cb42a3be88bc6df26445e2
Arib Alam <aribalam64@gmail.com>
2019-10-13 22:52:23 +0530

    set height of header to full width

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
 $(document).ready(function() {
     
     $('.header').height($(window).height());
-
-    // Set the section content height to full height
-    console.log(window.innerHeight);
-    $('.section_content').height(window.innerHeight);
-    $('.section_content').css('padding', 30);
 });
